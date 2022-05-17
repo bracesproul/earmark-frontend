edd2aa1ae0b17ba9b421aa2a7bf408ba15020f20
Brace Sproul <braceasproul@gmail.com>
2022-05-17 22:20:59 +0100

    updated dashboard page

--- pages/account/dashboard.tsx
+++ pages/account/dashboard.tsx
@@ -1,103 +1,20 @@
 /* eslint-disable */
-import React, {
-    useEffect,
+import React, { 
     useState,
+    useEffect,
 } from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import useCollapse from 'react-collapsed'
-import styles from '../../styles/Dashboard/Dashboard.module.css'
 import axios from 'axios';
-import { globalVars } from '../../src/lib/globalVars';
+import { useRouter } from 'next/router';
 import { useAuth } from '../../src/lib/hooks/useAuth';
-
-import { getAuth } from "firebase/auth";
-import { initializeApp } from "firebase/app";
-
-import { DataGrid, 
-    GridRowsProp,
-    GridColDef,
- } from '@mui/x-data-grid';
-
+import { globalVars } from '../../src/lib/globalVars';
 import SideNav from '../../src/components/Sidenav';
-import DashboardBody from '../../src/components/DashboardBody';
 import NotSignedIn from '../../src/components/Auth/NotSignedIn';
-import { unstable_ownerWindow } from '@mui/utils';
+import DataGridTransactions from '../../src/components/NewUIComponents/DataGridTransactions';
+import styles from '../../styles/Dashboard/Dashboard.module.css';
+import HeadTemplate from '../../src/components/Head';
 
-const FRONTEND_URL = globalVars().FRONTEND_URL;
 const API_URL = globalVars().API_URL;
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCOnXDWQ369OM1lW0VC5FdYE19q1ug0_dc",
-    authDomain: "earmark-8d1d3.firebaseapp.com",
-    projectId: "earmark-8d1d3",
-    storageBucket: "earmark-8d1d3.appspot.com",
-    messagingSenderId: "46302537330",
-    appId: "1:46302537330:web:403eac7f28d2a4868944eb",
-    measurementId: "G-5474KY2MRV"
-};
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-
-// DATA FOR STATIC SITE, REPLACE WITH DYNAMIC FETCHED DATA LATER
-
-const transactionData = [
-    { id: "ejradyRl9Mt9wKvjdow1fZDnvx4GwKi7wpx7E", col1: "McDonald's", col2: '2021-05-07', col3: "12.01", col4: "Food and Drink" },
-    { id: "ejrat9wKvjdow1fZDnvx4GwKi7wpx7E", col1: 'Apple', col2: "2021-06-01", col3: "2,199.99", col4: "Electronics" },
-    { id: "ejradyRl9Mt9wjwefjdow1fZDnvx4GwKi7wpx7E", col1: 'Safeway', col2: "2021-02-08", col3: "19.88", col4: "Grocries" },
-    { id: "ejra43gMt9wKvjdow1fZDnvx4GwKi7wpx7E", col1: 'Oori', col2: "2021-08-22", col3: "8.75", col4: "Food and Drink" },
-    { id: "ejradyRl9Mkfdow1fZDnvx4GwKi7wpx7E", col1: 'Target', col2: "2021-01-29", col3: "38.98", col4: "Shopping" },
-    { id: "ejKvjdow1fZDnvx4GwKi7wpx7E", col1: 'Home Depot', col2: "2021-12-31", col3: "133.22", col4: "Home Goods" },
-];
-
-const accountData = [
-    { id: "RLPVWZlv7wu7gEz5ArgbtDynKbxkzpiJ8aeWQ", col1: "Bank of America", col2: '588.12', col3: "Checking" },
-    { id: "ejrat9wKvjdow1fZDnvx4GwKi7wpx7E", col1: 'Chase', col2: "822.01", col3: "Savings" },
-    { id: "ejradyRl9Mt9wjwefjdow1fZDnvx4GwKi7wpx7E", col1: 'Fidelity', col2: "4,008.62", col3: "Investment" },
-    { id: "ejra43gMt9wKvjdow1fZDnvx4GwKi7wpx7E", col1: 'Plaid IRA', col2: "19,663.91", col3: "Investment" },
-];
-
-const TREEMAP_DATA = [
-	{
-		name: 'axis',
-		children: [
-			{ name: 'January', size: 100000, fill: "black" },
-			{ name: 'Febuary', size: 100000, fill: "Red" },
-			{ name: 'March', size: 100000, fill: "Orange" },
-			{ name: 'April', size: 100000, fill: "Yellow" },
-			{ name: 'May', size: 100000, fill: "Green" },
-            { name: 'June', size: 100000, fill: "Blue" },
-			{ name: 'July', size: 100000, fill: "Indigo" },
-			{ name: 'August', size: 100000, fill: "Violet" },
-			{ name: 'September', size: 100000, fill: "grey" },
-			{ name: 'October', size: 100000, fill: "pink" },
-			{ name: 'November', size: 100000, fill: "purple" },
-			{ name: 'December', size: 100000, fill: "brown" },
-		],
-	},
-];
-
-const BAR_CHART_DATA = [
-  { name: 'January', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'February', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'March', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'April', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'May', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'June', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'July', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'August', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'September', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'October', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'November', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-  { name: 'December', food: 250, transportation: 300, utilities: 350, leasure: 400 },
-];
-
-const PIE_CHART_DATA = [
-  { name: "Food", value: 100, fill: "purple" },
-  { name: "Transportation", value: 200, fill: "blue" },
-  { name: "Utilities", value: 300, fill: "red" },
-  { name: "Leasure", value: 400, fill: "green" },
-];
 
 export async function getServerSideProps({ req, res }) {
     const config = {
@@ -121,38 +38,53 @@ export async function getServerSideProps({ req, res }) {
         { field: 'col3', headerName: 'Amount', width: 150 },
         { field: 'col4', headerName: 'Category', width: 150 },
     ];
-    const data = [
-        {dataGridColumns: allTransactionsColumns},
-        {dataGridRows: axiosResponse.data.dataGridTransactions},
-        {transactionMetadata: axiosResponse.data.transactionMetadata}
-    ]
     return {
-      props: { data },
+      props: { 
+        dataGridColumns: allTransactionsColumns,
+        dataGridRows: axiosResponse.data.dataGridTransactions,
+        transactionMetadata: axiosResponse.data.transactionMetadata
+       },
     }
 }
 
-export default function Home({ data }) {
+
+const Dashboard = ({ dataGridColumns, dataGridRows, transactionMetadata }) => {
     const auth = useAuth();
-    console.log(data)
+    /* @ts-ignore */
+    if (!auth.user) return <NeedAuth />;
 
     return (
-        <div className="">
-        <Head>
-            <title>Dashboard</title>
-            <meta name="description" content="Dashboard overview for Earmark" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <div className={styles.dashboard}>
+        <HeadTemplate title="Dashboard" description="Dashboard overview for Earmark" iconPath="/favicon.ico" />
         <main className={styles.main}>
-            <SideNav />
-            <div className="dashboard-container">
-                
-                <div className="data-container">
-                    {/* @ts-ignore */}
-                    { !auth.user ? <NotSignedIn /> : <DashboardBody transactionData={transactionData} accountData={accountData} bar_chart={BAR_CHART_DATA} tree_map={TREEMAP_DATA} pie_chart={PIE_CHART_DATA} /> }
-                </div>
-            </div>
+            <section className={styles.sidenavContainer}>
+                <SideNav />
+            </section>
+            <section className={styles.dashboardContainer}>
+                <DataGridTransactions dataGridColumns={dataGridColumns} dataGridRows={dataGridRows} transactionMetadata={transactionMetadata} identifier="test" />
+            </section>
         </main>
         <footer></footer>
         </div>
     )
 }
+
+const NeedAuth = () => {
+    return (
+        <div className={styles.dashboard}>
+        <HeadTemplate title="Dashboard" description="Dashboard overview for Earmark" iconPath="/favicon.ico" />
+        <main className={styles.main}>
+            <section className={styles.sidenavContainer}>
+                <SideNav />
+            </section>
+            <section className={styles.dashboardContainer}>
+                <NotSignedIn />
+            </section>
+        </main>
+        <footer></footer>
+        </div>
+    )
+};
+
+export default Dashboard;
+
