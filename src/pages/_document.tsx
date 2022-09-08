8d141b547953c2fce8b9c37a297e41ee80fa54c6
Brace Sproul <braceasproul@gmail.com>
2022-09-08 02:44:18 +0100

    comented out _document.tsx page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
+/*
 import { Html, Head, Main, NextScript } from 'next/document'
 
-/*
+/!*
 * Keeping this for when I want to add things such as:
 * - custom fonts from Google Fonts
 * - google analytics
 * - other CDNs
-* */
+* *!/
 
 export async function getServerSideProps({ req, res }) {
     const cookieTheme = req.cookies['earmark-theme'] || 'dark';
@@ -22,4 +23,4 @@ export default function Document({ cookieTheme }) {
             </body>
         </Html>
     )
-}
+}*/
